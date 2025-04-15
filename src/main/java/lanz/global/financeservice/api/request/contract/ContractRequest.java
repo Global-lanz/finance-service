316aa0cf7b813e7d6c40dd3d39752d3dbf3554ee
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-15 11:47:17 +0100

    Fixed the same for integer values

--- src/main/java/lanz/global/financeservice/api/request/contract/ContractRequest.java
+++ src/main/java/lanz/global/financeservice/api/request/contract/ContractRequest.java
@@ -1,9 +1,10 @@
 package lanz.global.financeservice.api.request.contract;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -13,7 +14,7 @@ import java.util.UUID;
 public record ContractRequest(@Schema(description = "The ID of the customer") @NotNull UUID customerId,
                               @Schema(description = "The total amount of the contract", example = "299.90") @NotNull BigDecimal totalAmount,
                               @Schema(description = "The frequency of the payment", example = "MONTHLY") String frequency,
-                              @Schema(description = "The ID of the customer", example = "10") @Size(max = 31, min = 1) Integer paymentDay,
+                              @Schema(description = "The ID of the customer", example = "10") @DecimalMax(value = "31") @DecimalMin(value = "1") Integer paymentDay,
                               @Schema(description = "The ID of the customer", example = "2025-01-15") LocalDate start,
                               @Schema(description = "The ID of the customer", example = "2025-07-15") LocalDate end,
                               @Schema(description = "The ID of the customer") String terminationClause,
