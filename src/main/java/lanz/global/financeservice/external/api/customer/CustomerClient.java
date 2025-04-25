15c5d50687ef3181ef073571eac0e366d16f9513
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-25 15:10:13 +0100

    SCRUM-30 / Added feign client

---
+++ src/main/java/lanz/global/financeservice/external/api/customer/CustomerClient.java
@@ -0,0 +1,17 @@
+package lanz.global.financeservice.external.api.customer;
+
+import lanz.global.financeservice.external.api.customer.response.CustomerResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(name = "CUSTOMER-SERVICE")
+public interface CustomerClient {
+
+    @GetMapping("/customer/{customerId}")
+    ResponseEntity<CustomerResponse> findCustomerById(@PathVariable UUID customerId);
+
+}
