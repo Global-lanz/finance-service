52ece31f77344de4c7198d6398b79fca79cbba1f
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-06-25 14:03:23 +0100

    SCRUM-38 / Added customer data in the contract response

--- src/main/java/lanz/global/financeservice/api/response/contract/ContractResponse.java
+++ src/main/java/lanz/global/financeservice/api/response/contract/ContractResponse.java
@@ -2,23 +2,37 @@ package lanz.global.financeservice.api.response.contract;
 
 import lanz.global.financeservice.model.ContractStatusEnum;
 import lanz.global.financeservice.model.ContractTypeEnum;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record ContractResponse(UUID contractId,
-                               UUID customerId,
-                               BigDecimal totalAmount,
-                               String frequency,
-                               Integer paymentDay,
-                               LocalDate start,
-                               LocalDate end,
-                               ContractStatusEnum status,
-                               ContractTypeEnum type,
-                               String terminationClause,
-                               BigDecimal penaltyFee,
-                               String description,
-                               UUID currencyId) implements Serializable {
+@Getter
+@Setter
+public class ContractResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7142606946855707319L;
+
+    private UUID contractId;
+
+    private BigDecimal totalAmount;
+    private String frequency;
+    private Integer paymentDay;
+    private LocalDate start;
+    private LocalDate end;
+    private ContractStatusEnum status;
+    private ContractTypeEnum type;
+    private String terminationClause;
+    private BigDecimal penaltyFee;
+    private String description;
+    private ContractCustomerResponse customer;
+
+    @Deprecated(forRemoval = true, since = "1.5.0")
+    private UUID customerId;
+    private UUID currencyId;
 }
--- src/main/java/lanz/global/financeservice/util/converter/ContractResponseConverter.java
+++ src/main/java/lanz/global/financeservice/util/converter/ContractResponseConverter.java
@@ -1,25 +1,42 @@
 package lanz.global.financeservice.util.converter;
 
+import lanz.global.financeservice.api.response.contract.ContractCustomerResponse;
 import lanz.global.financeservice.api.response.contract.ContractResponse;
+import lanz.global.financeservice.external.api.customer.response.CustomerResponse;
 import lanz.global.financeservice.model.Contract;
+import lanz.global.financeservice.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ContractResponseConverter implements Converter<Contract, ContractResponse> {
+
+    private final CustomerService customerService;
+
     @Override
     public ContractResponse convert(Contract source) {
-        return new ContractResponse(source.getContractId(), source.getCustomerId(),
-                source.getTotalAmount(),
-                source.getFrequency(),
-                source.getPaymentDay(),
-                source.getStart(),
-                source.getEnd(),
-                source.getStatus(),
-                source.getType(),
-                source.getTerminationClause(),
-                source.getPenaltyFee(),
-                source.getDescription(),
-                source.getCurrencyId());
+        ContractResponse response = new ContractResponse();
+        response.setContractId(source.getContractId());
+        response.setCustomerId(source.getCustomerId());
+        response.setTotalAmount(source.getTotalAmount());
+        response.setFrequency(source.getFrequency());
+        response.setPaymentDay(source.getPaymentDay());
+        response.setStart(source.getStart());
+        response.setEnd(source.getEnd());
+        response.setStatus(source.getStatus());
+        response.setType(source.getType());
+        response.setTerminationClause(source.getTerminationClause());
+        response.setPenaltyFee(source.getPenaltyFee());
+        response.setDescription(source.getDescription());
+        response.setCurrencyId(source.getCurrencyId());
+
+        CustomerResponse customerResponse = customerService.findCustomerById(source.getCustomerId());
+
+        response.setCustomer(new ContractCustomerResponse(customerResponse.customerId(), customerResponse.name()));
+
+        return response;
     }
 }
