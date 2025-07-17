fb0212185541344c52451addaf654e5eb9224ce7
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-07-17 23:56:06 +0100

    Fixed GetContractParams class

--- src/main/java/lanz/global/financeservice/api/request/contract/GetContractParams.java
+++ src/main/java/lanz/global/financeservice/api/request/contract/GetContractParams.java
@@ -2,28 +2,27 @@ package lanz.global.financeservice.api.request.contract;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.Serial;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class GetContractParams extends Pageable {
+public class GetContractParams extends PageRequest {
+
+    @Serial
+    private static final long serialVersionUID = -3981791853063930213L;
 
     private UUID customerId;
 
-    public GetContractParams(int page, int size, List<String> sort) {
-        super(page, size, sort);
-    }
-
     public GetContractParams() {
-        super(0, 0, Collections.emptyList());
+        super(0, 10, Sort.unsorted());
     }
 
-    public GetContractParams(int page, int size, List<String> sort, UUID customerId) {
-        super(page, size, sort);
+    public GetContractParams(UUID customerId) {
+        super(0, 10, Sort.unsorted());
         this.customerId = customerId;
     }
 }
