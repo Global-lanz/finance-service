b3206c10720610597cb19cb0570f28a2c3122d75
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-07-20 19:42:47 +0100

    Fixed head interceptor

--- src/main/java/lanz/global/financeservice/security/HeaderInterceptor.java
+++ src/main/java/lanz/global/financeservice/security/HeaderInterceptor.java
@@ -6,9 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 @Component
 @RequiredArgsConstructor
 public class HeaderInterceptor implements RequestInterceptor {
@@ -17,8 +14,11 @@ public class HeaderInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        Stream.ofNullable(requestTemplate.headers())
-                .flatMap(headers -> headers.entrySet().stream())
-                .forEach(entry -> requestTemplate.header(entry.getKey(), entry.getValue()));
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> {
+            String headerValue = request.getHeader(headerName);
+            if (headerValue != null) {
+                requestTemplate.header(headerName, headerValue);
+            }
+        });
     }
 }
