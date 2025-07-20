f4fa71430e874d6a12c151db2fae524b538810c6
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-07-20 19:56:36 +0100

    SCRUM-40 / Added validation when removing customers with contracts (#9)
    * Fixed error
    
    * Fixed filtering
    
    * Fixed param
    
    * Fixed
    
    * Updated parameter
    
    * Fixed empty values
    
    * Added constructors
    
    * Added bad request handling
    
    * removed exception handling
    
    * Fixed Abstract Class
    
    * Fixed errors on criteria builder
    
    * Removed annotations
    
    * updated criteria builder
    
    * Test with predicates
    
    * Added persistence unit
    
    * Changed persistence unit
    
    * Added specifications
    
    * Removed unused class
    
    * Fixed GetContractParams class
    
    * Fixed injection
    
    * Fixed pagination
    
    * Removed all unused imports
    
    * Fixed paging order
    
    * Fixed offset
    
    * Fixed header interceptor
    
    * Fixed head interceptor

--- src/main/java/lanz/global/financeservice/service/CurrencyService.java
+++ src/main/java/lanz/global/financeservice/service/CurrencyService.java
@@ -4,14 +4,13 @@ import lanz.global.financeservice.exception.BadRequestException;
 import lanz.global.financeservice.model.Currency;
 import lanz.global.financeservice.repository.CurrencyRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
