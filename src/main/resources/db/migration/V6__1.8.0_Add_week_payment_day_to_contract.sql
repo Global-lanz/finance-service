49830b7aa501ed2fe922a1de2a9ae4eb7f14ef1e
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-07-22 23:50:54 +0100

    Added script to fix existing contracts

--- src/main/resources/db/migration/V6__1.8.0_Add_week_payment_day_to_contract.sql
+++ src/main/resources/db/migration/V6__1.8.0_Add_week_payment_day_to_contract.sql
@@ -1,2 +1,4 @@
 ALTER TABLE IF EXISTS contract
     ADD COLUMN week_payment_day CHARACTER VARYING;
+
+UPDATE contract SET frequency = 'MONTHLY' WHERE frequency NOT IN ('ONLY_ONCE', 'WEEKLY', 'ANNUALLY');
